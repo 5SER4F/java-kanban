50828c90b50695ed15d9d0059d772a244df4b5cb
Серафим <seraphim.uhanov@gmail.com>
2023-01-21 09:33:02 +0300

    test: добавить тесты в main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,40 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+        TaskManager taskManager = new TaskManager();
+
+        Task task1 = new Task("Задача 1", "Описание задачи 1", Status.NEW);
+        Task task2 = new Task("Задача 2", "Описание задачи 2", Status.DONE);
+//        taskManager.addTask(task1);
+//        taskManager.addTask(task2);
+//        taskManager.printTasks();
+//        taskManager.removeTask(task2.getId());
+//        taskManager.printTasks();
+
+        Epic epic = new Epic("Эпик", "Описание эпика 1");
+        taskManager.addEpic(epic);
+
+        Subtask subtask1 = new Subtask("Подзадача 1", "Описание подзадачи 1", Status.NEW, epic.getId());
+        taskManager.addSubtask(subtask1);
+
+//        taskManager.printEpics();
+//        taskManager.getSubtasksOfEpic(epic);
+
+        Subtask subtask2 = new Subtask("Подзадача 2", "Описание подзадачи 2", Status.IN_PROGRESS, epic.getId());
+        taskManager.addSubtask(subtask2);
+
+//        taskManager.printEpics();
+//        taskManager.getSubtasksOfEpic(epic);
+
+        taskManager.removeSubtask(subtask2.getId());
+
+        taskManager.printEpics();
+        taskManager.getSubtasksOfEpic(epic);
+
+        taskManager.removeEpic(epic.getId());
+
+        taskManager.printEpics();
+        taskManager.printSubtasks();
+
     }
 }
